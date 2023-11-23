e80cdf52c07037144318d67665da473f7cfc187f
Ariel Llerena <135856134+cuatrix2@users.noreply.github.com>
2023-11-23 16:55:11 -0500

    factoreo detallado

--- factorial.php
+++ factorial.php
@@ -54,10 +54,27 @@
 
         // Validar que se haya ingresado un número
         if (!is_numeric($numero)) {
-            echo "<p>Por favor, ingrese un número válido.</p>";
+            echo '<div class="result-container error"><p>Por favor, ingrese un número válido.</p></div>';
         } else {
-            $factorial = calcularFactorial($numero);
-            echo "<p>El factorial de $numero es $factorial.</p>";
+            echo '<div class="result-container">';
+            echo "<p>Proceso de cálculo del factorial para <strong>$numero</strong>:</p>";
+            
+            // Mostrar el número ingresado
+            echo "<p>Paso 0: Se ha ingresado el número <strong>$numero</strong>.</p>";
+
+            // Inicializar el resultado
+            $resultado = 1;
+
+            // Calcular el factorial
+            for ($i = 1; $i <= $numero; $i++) {
+                $resultado *= $i;
+                echo "<p>Paso $i: Multiplicar por $i, el resultado es <strong>$resultado</strong>.</p>";
+            }
+
+            // Mostrar el resultado final
+            echo "<p>Paso " . ($numero + 1) . ": El factorial de <strong>$numero</strong> es <strong>$resultado</strong>.</p>";
+            
+            echo '</div>';
         }
     }
     ?>
